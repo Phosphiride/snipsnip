a3e28cb111dabb103d1518e5b3bab52ad359f895
Phosphiride <64817854+Phosphiride@users.noreply.github.com>
2020-08-21 20:42:50 -0400

    imported rebase data to dataframe

---
+++ notebooks/playground.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 20:47:15 2020
+
+@author: yujia
+"""
+
+def seq_test():
+    "test"
+    return: "test"
---
+++ notebooks/restrict_main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 31 18:16:11 2020
+
+@author: yujia
+"""
+
+import pandas as pd
+import Bio
+import re
+import lark as lk
+
+data = pd.read_csv("C:\\Users\\yujia\\projects\\snipsnip\\data_files\\restrict_data_2.csv", sep=";")
+
+
+def find_enzyme():
+    return
+
+def convert_regex():
+    rec_seq = data["recognition sequence"]
+    
+    return
+
+print(data)
